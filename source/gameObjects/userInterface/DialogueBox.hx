184be0bcec298a6388b089862206e902e3bfb575
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-10-23 16:34:45 -0300

    ata

--- source/gameObjects/userInterface/DialogueBox.hx
+++ source/gameObjects/userInterface/DialogueBox.hx
@@ -230,7 +230,7 @@ class DialogueBox extends FlxSpriteGroup
 			var boxJson = Paths.file('images/dialogue/boxes/$curBox/$curBox.json');
 
 			// load the json and sprite
-			boxData = haxe.Json.parse(sys.io.File.getContent(boxJson));
+			boxData = haxe.Json.parse(openfl.utils.Assets.getText(boxJson));
 			box.frames = Paths.getSparrowAtlas('dialogue/boxes/$curBox/$curBox');
 
 			// get the states sectioon
@@ -318,7 +318,7 @@ class DialogueBox extends FlxSpriteGroup
 				// load the json file
 				if (openfl.utils.Assets.exists(portraitJson))
 				{
-					portraitData = haxe.Json.parse(sys.io.File.getContent(portraitJson));
+					portraitData = haxe.Json.parse(openfl.utils.Assets.getText(portraitJson));
 					portrait.frames = Paths.getSparrowAtlas('dialogue/portraits/$curCharacter/$curCharacter');
 				}
 
