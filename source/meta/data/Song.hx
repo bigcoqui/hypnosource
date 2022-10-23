50d07bcd22d2cbb00c159286bd011d97007459dc
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-10-23 09:04:07 -0300

    buceta

--- source/meta/data/Song.hx
+++ source/meta/data/Song.hx
@@ -53,7 +53,7 @@ class Song
 		if (library != null)
 			rawJson = Assets.getText(Paths.songJson(folder.toLowerCase(), jsonInput.toLowerCase(), PlayState.old, PlayState.songLibrary)).trim();
 		else 
-			rawJson = OpenFlAssets.getTxt(Paths.songJson(folder.toLowerCase(), jsonInput.toLowerCase(), PlayState.old, PlayState.songLibrary)).trim();
+			rawJson = OpenFlAssets.getText(Paths.songJson(folder.toLowerCase(), jsonInput.toLowerCase(), PlayState.old, PlayState.songLibrary)).trim();
 
 		while (!rawJson.endsWith("}"))
 			rawJson = rawJson.substr(0, rawJson.length - 1);
