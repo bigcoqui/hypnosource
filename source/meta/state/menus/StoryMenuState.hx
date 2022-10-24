4f9a79a8956f46b9bfd043362dc2516a1b624e73
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-10-24 15:26:51 -0300

    update StoryMenuState.hx

--- source/meta/state/menus/StoryMenuState.hx
+++ source/meta/state/menus/StoryMenuState.hx
@@ -12,7 +12,6 @@ import gameObjects.Character;
 import meta.MusicBeat.MusicBeatState;
 import meta.data.Highscore;
 import meta.data.Song;
-import meta.data.dependency.Discord;
 import overworld.OverworldStage;
 
 using StringTools;
